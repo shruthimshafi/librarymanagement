05cdf71876b3a5cbc728231026f1693352ca66e6
shruthimshafi <32947625+shruthimshafi@users.noreply.github.com>
2017-11-13 15:13:51 +0530

    Update settings.py

--- mylibrary/settings.py
+++ mylibrary/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+#Changes by AJay
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
